dfe85a85b9fcfc65b6d2d4c7577cb31705fad455
buyaramadhan <ytchochote@gmail.com>
2025-05-24 15:42:10 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile',[ProfileController::class, 'edit'])->name('profile.edit');
+});
+
 // Authenticated User Routes
 Route::middleware('auth')->group(function () {
     // Profile Routes
